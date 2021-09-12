15d17e7dbf25245d3db3a652b241fcebf876a0c4
Drago Stevanovic <drago@sounder.fm>
2021-09-12 15:52:11 +0300

    Authetication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\AuthController;
 use App\Http\Middleware\CheckForDuplicates;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,8 +21,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::post('/movies', [MovieController::class, 'store'])->middleware(CheckForDuplicates::class);
+    Route::put('/movies/{movie}', [MovieController::class, 'update']);
+    Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
+});
+
+
+
 Route::get('/movies', [MovieController::class, 'index']);
 Route::get('/movies/{movie}', [MovieController::class, 'show']);
-Route::post('/movies', [MovieController::class, 'store'])->middleware(CheckForDuplicates::class);
-Route::put('/movies/{movie}', [MovieController::class, 'update']);
-Route::delete('/movies/{movie}', [MovieController::class, 'destroy']);
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+    Route::get('/me', [AuthController::class, 'getMyProfile'])->middleware('auth');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/refresh', [AuthController::class, 'refreshToken']);
+});
